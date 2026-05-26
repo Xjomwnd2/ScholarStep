cf393e77bee8eab81b414695c39258828122d354
Joel Ndiba Mwaura <Xjomwnd2@users.noreply.github.com>
2026-05-26 21:09:21 +0300

    commit

--- auth.js
+++ auth.js
@@ -2,10 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // Bearer <token>
-
+  const token = authHeader && authHeader.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'No token provided' });
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
