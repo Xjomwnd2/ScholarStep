2fcf23f3907f5645619bbdb3d6295292ca7a6f0c
Joel Ndiba Mwaura <Xjomwnd2@users.noreply.github.com>
2026-06-01 15:17:54 +0000

    Jun 1, 2026, 6:17 PM

---
+++ backend/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../db');
+
+const router = express.Router();
+
+/**
+ * REGISTER USER
+ */
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check if user exists
+    const user
