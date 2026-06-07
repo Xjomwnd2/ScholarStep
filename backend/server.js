7108aed216952050e6ca37eec55910b5044317b9
Joel Ndiba Mwaura <Xjomwnd2@users.noreply.github.com>
2026-06-08 01:44:50 +0300

    commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'ScholarStep API is running', version: '1.0.0' });
+});
+
+app.get('/api/scholarships', (req, res) => {
+  res.json({ scholarships: [], message: 'Scholarships endpoint ready' });
+});
+
+app.listen(PORT, () => {
+  console.log('ScholarStep backend running on http://localhost:' + PORT);
+});
